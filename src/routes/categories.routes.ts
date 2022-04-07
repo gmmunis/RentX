b73a9ca01d5724298c4fd5ce62876197b8f8f601
guilherme.munis <guilherme.munis@compasso.com.br>
2022-04-07 14:15:19 -0300

    Applying S.O.L.I.D principles (SRP and DIP)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryServices } from "../services/CreateCategoryServices";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -7,15 +8,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    const createCategoryServices = new CreateCategoryServices(categoriesRepository);
+    createCategoryServices.execute({ name, description });
 
-    if(categoryAlreadyExists) {
-        return response.status(400).json({ error: "Category already exists!" });
-    }
-
-    categoriesRepository.create({ name, description });
-
-    return response.status(201).send()
+    return response.status(201).send();
 });
 
 categoriesRoutes.get("/", (request, response) => {
