4c44bbd2a58916153476244deebfa07a5f8780a4
guilherme munis <gmmunis@hotmail.com>
2022-04-25 17:21:43 -0300

    Alter table User - remove username column

--- src/modules/accounts/repositories/implementations/IUsersRepository.ts
+++ src/modules/accounts/repositories/implementations/IUsersRepository.ts
@@ -2,7 +2,7 @@ import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { User } from "../../entities/User";
 
 interface IUsersRepository {
-  create({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void>;
+  create({ name, email, password, driver_license }: ICreateUserDTO): Promise<void>;
 }
 
 export { IUsersRepository };
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -8,8 +8,8 @@ class CreateUserUseCase {
     @inject("UsersRepository")
     private usersRepository: IUsersRepository) {}
 
-    async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void> {
-      await this.usersRepository.create({ name, username, email, password, driver_license });
+    async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+      await this.usersRepository.create({ name, email, password, driver_license });
     }
 }
 
