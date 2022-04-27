c68846f96394ad266975b67110b147111be8dd81
guilherme munis <gmmunis@hotmail.com>
2022-04-27 16:47:08 -0300

    User token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,54 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/implementations/IUsersRepository";
+
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse>{
+    const user = await this.usersRepository.findByEmail(email);
+
+    if(!user){
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "a7e071b3de48cec1dd24de6cbe6c7bf1", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+
+export { AuthenticateUserUseCase };
