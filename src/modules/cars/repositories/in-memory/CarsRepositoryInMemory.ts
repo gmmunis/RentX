5966577ee8b40cb474a7ebc498a52cb839574c72
gmmunis <gmmunis@hotmail.com>
2022-05-25 10:45:08 -0300

    Create Car useCases

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { ICarsRepository } from "@modules/cars/infra/typeorm/repositories/ICarsRepository";
+
+class CarsRepositoryInMemory implements ICarsRepository {
+    cars: Car[] = [];
+    
+    async create({ name, description, daily_rate, license_plate, fine_amount, brand, category_id }: ICreateCarDTO): Promise<Car> {
+        const car = new Car();
+
+        Object.assign(car, {
+            brand,
+            category_id,
+            daily_rate,
+            description,
+            fine_amount,
+            name,
+            license_plate
+        });
+
+        this.cars.push(car);
+
+        return car;
+    }
+
+    async findByLicensePlate(license_plate: string): Promise<Car> {
+        return this.cars.find((car) => car.license_plate === license_plate);
+    }
+}
+
+export { CarsRepositoryInMemory };
