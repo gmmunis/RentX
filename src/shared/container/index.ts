44ee4a96f6b5d7c9e40a89c0d2ca7f56d737005a
gmmunis <gmmunis@hotmail.com>
2022-05-28 14:56:26 -0300

    Structuring car entity

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,8 +5,11 @@ import { ICategoriesRepository } from "@modules/cars/infra/typeorm/repositories/
 import { SpecificationsRepository } from "@modules/cars/repositories/SpecificationsRepository";
 import { IUsersRepository } from "@modules/accounts/infra/typeorm/repositories/IUsersRepository";
 import { ISpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/ISpecificationsRepository";
+import { ICarsRepository } from "@modules/cars/infra/typeorm/repositories/ICarsRepository";
+import { CarsRepository } from "@modules/cars/repositories/CarsRepository";
 
 container.registerSingleton<ICategoriesRepository>("CategoriesRepository", CategoriesRepository);
 container.registerSingleton<ISpecificationsRepository>("SpecificationsRepository", SpecificationsRepository);
 container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 
