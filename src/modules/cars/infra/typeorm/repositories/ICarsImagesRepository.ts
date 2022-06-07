e4fab99ba4b331b4a559ccb9cdc651b043afc1ed
gmmunis <gmmunis@hotmail.com>
2022-06-07 09:24:25 -0300

    Car Image UseCase

--- src/modules/cars/infra/typeorm/repositories/ICarsImagesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/ICarsImagesRepository.ts
@@ -1,7 +1,7 @@
 import { CarImage } from "../entities/CarImage";
 
-interface IUploadCarsImageRepository {
+interface ICarsImagesRepository {
     create(car_id: string, image_name: string): Promise<CarImage>;
 }
 
-export { IUploadCarsImageRepository };
+export { ICarsImagesRepository };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,11 +5,11 @@ import { ICategoriesRepository } from "@modules/cars/infra/typeorm/repositories/
 import { SpecificationsRepository } from "@modules/cars/repositories/SpecificationsRepository";
 import { IUsersRepository } from "@modules/accounts/infra/typeorm/repositories/IUsersRepository";
 import { ISpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/ISpecificationsRepository";
-import { ICarsRepository } from "@modules/cars/infra/typeorm/repositories/ICarsRepository";
-import { CarsRepository } from "@modules/cars/repositories/CarsRepository";
+import { ICarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "@modules/cars/repositories/CarsImagesRepository";
 
 container.registerSingleton<ICategoriesRepository>("CategoriesRepository", CategoriesRepository);
 container.registerSingleton<ISpecificationsRepository>("SpecificationsRepository", SpecificationsRepository);
 container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
-container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImagesRepository);
 
