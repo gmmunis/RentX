85813d1e46efce487f63173f6504a872898a6c33
gmmunis <gmmunis@hotmail.com>
2022-06-22 15:35:09 -0300

    Refresh Token Controller

--- src/modules/accounts/infra/typeorm/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/IUsersTokensRepository.ts
@@ -3,6 +3,8 @@ import { Usertokens } from "../entities/UserTokens";
 
 interface IUsersTokensRepository {
     create({ expires_date, refresh_token, user_id}: ICreateUserTokenDTO): Promise<Usertokens>;
+    findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<Usertokens>;
+    deleteById(id: string): Promise<void>;
 }
 
 export { IUsersTokensRepository };
