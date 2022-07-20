f1481c0f52bad15bb6e6652a97c0d8c8159bb015
gmmunis <gmmunis@hotmail.com>
2022-07-20 11:46:38 -0300

    S3 Upload Provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,8 +4,15 @@ import { DayjsDateProvider } from "./DateProvider/Implementations/DayjsDateProvi
 import { IMailProvider } from "./Mailprovider/IMailProvider";
 import { EtherealMailProvider } from "./Mailprovider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DayjsDateProvider", DayjsDateProvider);
 container.registerInstance<IMailProvider>("EtherealMailProvider", new EtherealMailProvider());
-container.registerSingleton<IStorageProvider>("StorageProvider", LocalStorageProvider);
+
+const diskStorage = {
+    local: LocalStorageProvider,
+    s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>("StorageProvider", diskStorage[process.env.disk]);
